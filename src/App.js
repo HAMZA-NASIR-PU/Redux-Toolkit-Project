c83a5b1071ea6a3966a1677da1677c3d65a3ce64
HAMZA-NASIR-PU <hamzanasir1111.hn@gmail.com>
2024-03-27 22:06:38 +0500

    clean project and restart practice of redux toolkit

--- src/App.js
+++ src/App.js
@@ -1,12 +1,8 @@
 import './App.css';
-import AddPostForm from './features/posts/AddPostForm';
-import PostsList from './features/posts/PostsList';
 
 function App() {
   return (
     <div className="App">
-      <AddPostForm />
-      <PostsList />
     </div>
   );
 }
