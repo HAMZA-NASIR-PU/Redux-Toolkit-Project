e5ab1e83b3399a73d8c0b8970c25599284b93581
HAMZA-NASIR-PU <hamzanasir1111.hn@gmail.com>
2024-11-07 10:48:11 +0500

    Complete project without using redux toolkit

--- src/components/Product.js
+++ src/components/Product.js
@@ -1,7 +1,41 @@
+import { useState, useEffect } from "react";
+import Button from 'react-bootstrap/Button';
+import Card from 'react-bootstrap/Card';
+
 const Product = () => {
+    const [products, getProducts] = useState([]);
+
+    useEffect(() => {
+        fetch('https://fakestoreapi.com/products')
+            .then(data => data.json())
+            .then(result => getProducts(result));
+    }, []);
+
+    const cards = products.map(product => (
+        <div key={product.id} className="col-md-3" style={{ marginBottom: '10px' }}>
+            <Card className="h-100">
+                <div className="text-center">
+                    <Card.Img variant="top" src={product.image} style={{ width: '100px', height: '130px' }} />
+                </div>
+                <Card.Body>
+                    <Card.Title>{product.title}</Card.Title>
+                    <Card.Text>
+                        INR. {product.price}
+                    </Card.Text>
+                </Card.Body>
+                <Card.Footer style={{ backgroundColor: 'white' }}>
+                    <Button variant="primary">Add to Cart</Button>
+                </Card.Footer>
+            </Card>
+        </div>
+    ));
+
     return (
         <>
             <h1>Product Dashboard</h1>
+            <div className="row">
+                {cards}
+            </div>
         </>
     )
 }
---
+++ src/components/RootLayout.js
@@ -0,0 +1,18 @@
+import { Outlet } from "react-router-dom";
+import NavBar from "./NavBarPanel";
+
+const RootLayout = () => {
+
+    return (
+        <>
+            <NavBar />
+
+            <main>
+                <Outlet />
+            </main>
+
+        </>
+    )
+}
+
+export default RootLayout;
